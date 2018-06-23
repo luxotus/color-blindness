fd388027c8827d15c23b00ef4b867b41cdc67235
Felix Manuel <luxotus@gmail.com>
2018-06-23 18:31:49 -0400

    color toggle html and styling is now being generated correctly by js, next have to attach event listeners

--- color-blindness.js
+++ color-blindness.js
@@ -8,27 +8,25 @@ var colorBlindnessTool = (function() {
     let buildColorToggle = function () {
         let randomNum = Math.floor(Math.random() * 1000000000);
         let colorToggleHtml = '<div id="def-holder'+randomNum+'"><div class="button'+randomNum+'"><div class="arrow-lt'+randomNum+'"></div></div><div class="def'+randomNum+'">Deficiency</div><div class="button'+randomNum+'"><div class="arrow-rt'+randomNum+'"></div></div></div>';
-        let colorToggleStyle = '<style>';
+        let colorToggleStyle = '';
         let selectors = {
             id: {
                 '#def-holder': 'display:block;z-index:99999999;height:35px;width:200px;position:absolute;top:0;margin:auto;left:0;right:0;border:1px solid #b7b7b7;border-top:none;border-bottom-left-radius:5px;border-bottom-right-radius:5px;background-color:#fff;',
             },
             class: {
                 '.button': 'position:relative;display:flex;align-items:center;justify-content:center;height:100%;width:25px;float:left;cursor:pointer;',
-                '.arrow-lt': 'height:5px;width:5px;border-top:2pxsolidgray;border-right:2pxsolidgray;transform:rotate(-135deg);',
-                '.arrow-rt': 'height:5px;width:5px;border-top:2pxsolidgray;border-right:2pxsolidgray;transform:rotate(45deg);',
+                '.arrow-lt': 'height:5px;width:5px;border-top:2px solid gray;border-right:2px solid gray;transform:rotate(-135deg);',
+                '.arrow-rt': 'height:5px;width:5px;border-top:2px solid gray;border-right:2px solid gray;transform:rotate(45deg);',
                 '.def': 'height:100%;width:150px;position:relative;float:left;line-height:35px;font-size:20px;font-weight:bold;text-align:center;letter-spacing:0.5px;'
             }
         };
 
         for (const attribute in selectors) {
             for (const key in selectors[attribute]) {
-                colorToggleStyle += key+randomNum+' {'+selectors[attribute][key];
+                colorToggleStyle += key+randomNum+' {'+selectors[attribute][key] + '}';
             }
-            colorToggleStyle += '}';
         }
 
-        colorToggleStyle += '</style>';
         return {html: colorToggleHtml, style: colorToggleStyle};
     };
 
@@ -44,6 +42,15 @@ var colorBlindnessTool = (function() {
         if (typeof showDebugMessages == 'boolean') {
             debugMode = showDebugMessages;
         }
+
+        let head = document.head || document.getElementsByTagName('head')[0];
+        let body = document.getElementsByTagName('body');
+        let style = document.createElement('style');
+        let colorToggle = buildColorToggle();
+        style.type = 'text/css';
+        style.appendChild(document.createTextNode(colorToggle.style));
+        head.appendChild(style);
+        document.body.innerHTML += colorToggle.html;
 
         return '';
     };
