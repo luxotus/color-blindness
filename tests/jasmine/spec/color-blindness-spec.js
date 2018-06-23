e46c553142919322c2a65ed2327a7ab6a00e0fd1
Felix Manuel <luxotus@gmail.com>
2018-06-22 21:50:42 -0400

    Added comments on a general direction for this project

---
+++ tests/jasmine/spec/color-blindness-spec.js
@@ -0,0 +1,11 @@
+// Get details on a/all color deficiency/deficiencies 
+
+// Convert color to a specified color deficiency and convert it into specified deficiency
+
+// Convert colors on an image to a specified deficiency
+
+// Grab the color of an element on the page and convert its colors/images into a specified color deficiency 
+
+// Iterate through the all elements in a dom and pass it to funcition above
+
+// Setup event listeners on deficiency toggle buttons to loop through deficiencies
