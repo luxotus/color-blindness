1f66e0be7c8709dc9e9f9f375683f6ae9ba9ba28
Felix Manuel <luxotus@gmail.com>
2018-06-23 21:07:11 -0400

    setup the event listeners and displaying info. Time for the next function

--- color-blindness.js
+++ color-blindness.js
@@ -7,17 +7,17 @@ var colorBlindnessTool = (function() {
      */
     let buildColorToggle = function () {
         let randomNum = Math.floor(Math.random() * 1000000000);
-        let colorToggleHtml = '<div id="def-holder'+randomNum+'"><div class="button'+randomNum+'"><div class="arrow-lt'+randomNum+'"></div></div><div class="def'+randomNum+'">Deficiency</div><div class="button'+randomNum+'"><div class="arrow-rt'+randomNum+'"></div></div></div>';
+        let colorToggleHtml = '<div id="def-holder'+randomNum+'"><div id="button-lt'+randomNum+'" class="button'+randomNum+'"><div class="arrow-lt'+randomNum+'"></div></div><div id="def-txt'+randomNum+'" class="def'+randomNum+'">Deficiency</div><div id="button-rt'+randomNum+'" class="button'+randomNum+'"><div class="arrow-rt'+randomNum+'"></div></div></div>';
         let colorToggleStyle = '';
         let selectors = {
             id: {
-                '#def-holder': 'display:block;z-index:99999999;height:35px;width:200px;position:absolute;top:0;margin:auto;left:0;right:0;border:1px solid #b7b7b7;border-top:none;border-bottom-left-radius:5px;border-bottom-right-radius:5px;background-color:#fff;',
+                '#def-holder': 'display:block;z-index:99999999;height:35px;width:350px;position:absolute;top:0;margin:auto;left:0;right:0;border:1px solid #b7b7b7;border-top:none;border-bottom-left-radius:5px;border-bottom-right-radius:5px;background-color:#fff;'
             },
             class: {
                 '.button': 'position:relative;display:flex;align-items:center;justify-content:center;height:100%;width:25px;float:left;cursor:pointer;',
                 '.arrow-lt': 'height:5px;width:5px;border-top:2px solid gray;border-right:2px solid gray;transform:rotate(-135deg);',
                 '.arrow-rt': 'height:5px;width:5px;border-top:2px solid gray;border-right:2px solid gray;transform:rotate(45deg);',
-                '.def': 'height:100%;width:150px;position:relative;float:left;line-height:35px;font-size:20px;font-weight:bold;text-align:center;letter-spacing:0.5px;'
+                '.def': 'height:100%;width:300px;position:relative;float:left;line-height:35px;font-size:20px;font-weight:bold;text-align:center;letter-spacing:0.5px;'
             }
         };
 
@@ -27,7 +27,7 @@ var colorBlindnessTool = (function() {
             }
         }
 
-        return {html: colorToggleHtml, style: colorToggleStyle};
+        return {html: colorToggleHtml, style: colorToggleStyle, arrowLeftBtn: 'button-lt'+randomNum, arrowRightBtn: 'button-rt'+randomNum, txtId: 'def-txt'+randomNum};
     };
 
     /**
@@ -39,18 +39,48 @@ var colorBlindnessTool = (function() {
      * @returns {string} status message
      */
     let init = function ( showColorToggle, colorTogglePosition, showDebugMessages ) {
+        let style = document.createElement('style');
+        let colorToggle = buildColorToggle();
+        let deficiencyIndex = 0;
+        let colorDeficiencies = [
+            'Red-Weak/Protanomaly',
+            'Green-Weak/Deuteranomaly',
+            'Blue-Weak/Tritanomaly',
+            'Red-Blind/Protanopia',
+            'Green-Blind/Deuteranopia',
+            'Blue-Blind/Tritanopia',
+            'Monochromacy/Achromatopsia',
+            'Blue Cone Monochromacy'
+        ];
+        style.type = 'text/css';
+        style.appendChild(document.createTextNode(colorToggle.style));
+        document.getElementsByTagName('head')[0].appendChild(style);
+        document.getElementsByTagName('body')[0].innerHTML += colorToggle.html;
+        document.getElementById(colorToggle.txtId).innerHTML = colorDeficiencies[0];
+
         if (typeof showDebugMessages == 'boolean') {
             debugMode = showDebugMessages;
         }
 
-        let head = document.head || document.getElementsByTagName('head')[0];
-        let body = document.getElementsByTagName('body');
-        let style = document.createElement('style');
-        let colorToggle = buildColorToggle();
-        style.type = 'text/css';
-        style.appendChild(document.createTextNode(colorToggle.style));
-        head.appendChild(style);
-        document.body.innerHTML += colorToggle.html;
+        document.getElementById(colorToggle.arrowLeftBtn).addEventListener("click", function() {
+            if (deficiencyIndex == 0) {
+                deficiencyIndex = colorDeficiencies.length - 1;
+            } else {
+                deficiencyIndex--;
+            }
+
+            document.getElementById(colorToggle.txtId).innerHTML = colorDeficiencies[deficiencyIndex];
+        });
+
+        document.getElementById(colorToggle.arrowRightBtn).addEventListener("click", function() {
+            if (deficiencyIndex == colorDeficiencies.length - 1) {
+                deficiencyIndex = 0;
+            } else {
+                deficiencyIndex++;
+            }
+
+            document.getElementById(colorToggle.txtId).innerHTML = colorDeficiencies[deficiencyIndex];
+        });
 
         return '';
     };
